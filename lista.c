afc66964473f1e575ced127fe7be666da38d300a
jscsegura <jsc.segura@gmail.com>
2017-05-24 07:23:57 -0600

    Reparado el Segmentation Fault. Se comenta un poco el codigo

--- lista.c
+++ lista.c
@@ -14,6 +14,8 @@ return head;
 //--------------------------------------
 void printList(pos_t* head){
 
+
+/* Dicha función itera sobre toda la lista e imprime la misma en orden */
 pos_t* actual= head;
 
 while (actual != NULL){
@@ -24,16 +26,21 @@ actual= actual->next;
 }
 //----------------------------------------
 
+/* Dicha función intruduce un nuevo elemento al final de la lista */
 int push_back(pos_t* head, int new_value){
 
-pos_t* actual= head;
 
+pos_t* actual= head; //!< Variable local para apuntar al primer elemento
+
+
+
+ /* Se itera sobre la lista hasta que se apunte al último elemento */
 while (actual->next != NULL){
-actual= actual->next;
+actual= actual->next; 
 }
 
 actual -> next=malloc(sizeof(pos_t));
-
+/* En caso de que no haya espacio, se devuelve un uno. */
 	if (actual -> next==NULL){
 
 		return 1;
@@ -50,12 +57,21 @@ actual -> next=malloc(sizeof(pos_t));
 }
 //---------------------------------------
 
+/* Dicha función intruduce un nuevo elemento al frente de la lista */
 int push_front(pos_t** head, int new_value) {
+
+
     pos_t * nuevo;
+
+
+/* Damos un espacio de memoria a dicha lista */
     nuevo = malloc(sizeof(pos_t));
+
+/* En caso de que no haya espacio, se devuelve un uno. */
 	if (nuevo==NULL){
 		
 		return 1;
+
 
 	}
 	else{
@@ -71,10 +87,33 @@ int push_front(pos_t** head, int new_value) {
 
 int pop_back(pos_t* head){
 
-if(head->next==NULL){ //En caso de que haya solo un único elemento
+/* Se debe considerar si hay un solo elemento en la lista */
 
+	if(head->next==NULL){ 
+
+	head-> data=0;
+	free(head);
+	return 0;
+	}
+
+pos_t* actual= head;
+
+/* Se va a la última posición */
+
+	while (actual-> next -> next != NULL){ 
+		actual= actual->next; //Apuntamos a la última dirección
+	}
+
+
+actual -> next -> data=0;
+free(actual -> next);
+actual -> next =NULL;
+
+return 0;
 
 
 }
 
-}
+//--------------------------------------------
+
+
--- main.c
+++ main.c
@@ -11,10 +11,11 @@ push_back(a,4);
 pos_t** ptr=&a;
 push_front(ptr,3);
 
-
-
 printList(a);
 
+
+pop_back(a);
+printList(a);
 
 return 0;
 }
