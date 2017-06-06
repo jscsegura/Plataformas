20e652b663ed730fc4d45f8bdc06eefe2af172cd
jscsegura <jsc.segura@gmail.com>
2017-06-06 12:41:02 -0600

    Ahora si se resuelve, pero quedan terminos adicionales

--- Lab8/Laberinto.py
+++ Lab8/Laberinto.py
@@ -15,7 +15,7 @@ def traductor(laberinto):
                     a[i][j] = 1
                 elif laberinto[i][j]==' ':
                     a[i][j] = 0
-                elif laberinto[i][j]=='S':
+                elif laberinto[i][j]=='E':
                     a[i][j] = 2
                 else:
                     a[i][j] = 0
@@ -34,6 +34,8 @@ def traductorFinal(a,laberinto):
                     final[i][j] = 'S'
                 elif a[i][j]==3:
                     final[i][j] = 'o'
+        final[5][34]='S'
+        final[1][1]='E'
         return final
 
 
@@ -48,31 +50,33 @@ def search(x, y):
 
     a[x][y] = 3
 
-    if ((x < len(a)-1 and search(x+1, y))
+    if ((x > 0 and search(x-1, y))
+        or (y < len(a)-1 and search(x, y+1))
         or (y > 0 and search(x, y-1))
-        or (x > 0 and search(x-1, y))
-        or (y < len(a)-1 and search(x, y+1))):
+        or (x < len(a)-1 and search(x+1, y))):
         return True
 
     return False
+
+def imprimirText():
+    datos=open('laberintofinal.txt', 'w')
+
+    for i in range(7):
+        for j in range(36):
+            temp=final[i][j]
+            datos.write(temp)
+
+        datos.write("\n")
+
+
+    datos.close()
 
 
 
 laberinto= readPath()
 a=traductor(laberinto)
-search(1, 1)
+search(5, 34)
 print(a)
 final= traductorFinal(a, laberinto)
 print(final)
-
-datos=open('laberintofinal.txt', 'w')
-
-for i in range(7):
-    for j in range(36):
-        temp=final[i][j]
-        datos.write(temp)
-
-    datos.write("\n")
-
-
-datos.close()
+imprimirText()
